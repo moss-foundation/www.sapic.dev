6e5fc7d6de2942b8b5f840006fb5b25b824c4a63
EnthusedFear <enthceph@gmail.com>
2025-10-28 16:12:12 +0200

    fix some border inconsistencies

--- src/pages/Home/sections/FAQSection/FaqSection.tsx
+++ src/pages/Home/sections/FAQSection/FaqSection.tsx
@@ -1,8 +1,8 @@
+import FAQSectionImage from "@/assets/images/FAQSectionImage.webp";
+import { Accordion } from "@/components/Accordion";
 import LayoutContainer from "@/components/LayoutContainer";
 import Line from "@/components/Line";
 import FAQHeader from "./FAQHeader";
-import FAQSectionImage from "@/assets/images/FAQSectionImage.webp";
-import { Accordion } from "@/components/Accordion";
 
 const FaqSection = () => {
     return (
@@ -10,7 +10,7 @@ const FaqSection = () => {
             <LayoutContainer className="py-12 border-l border-r  border-dashed border-(--default-border-color) bg-[#fdfdf8]">
                 <FAQHeader />
                 <div className="grid grid-cols-2 grid-rows-[repeat(10,minmax(0,auto))] border-b border-dashed border-(--default-border-color)">
-                    <p className="text-6xl font-serif font-medium text-black text-balance leading-tight text-left row-span-3 border-b border-dashed border-(--default-border-color) pt-[60px] pb-[30px]">
+                    <p className="row-span-3 text-6xl font-serif font-medium text-black text-balance leading-tight text-left border-b border-dashed border-(--default-border-color) pt-[60px] pb-[30px]">
                         Answers for easy <span className="text-blue-4">understand</span>
                     </p>
 
@@ -21,7 +21,6 @@ const FaqSection = () => {
                             </Accordion>
                         </div>
                     </div>
-
                     <div className="row-start-2 col-start-2 flex items-center border-l border-b border-dashed border-(--default-border-color) pl-[26px] py-[26px] pr-5">
                         <Accordion title="Working with resources">
                             <p>
@@ -58,13 +57,17 @@ const FaqSection = () => {
                         </Accordion>
                     </div>
 
-                    <div className="row-start-6 row-span-5 col-start-1 border-t border-r border-dashed border-(--default-border-color) p-3">
+                    <div className="row-start-4 row-span-2 col-start-1 border-b border-dashed border-(--default-border-color)" />
+
+                    <div className="row-start-6 row-span-5 col-start-1  p-3">
                         <img
                             src={FAQSectionImage}
                             alt="FAQ Section Image"
                             className="w-full h-full object-contain pointer-events-none"
                         />
                     </div>
+
+                    <div className="row-start-6 row-span-5 col-start-2 border-l border-dashed border-(--default-border-color)" />
                 </div>
             </LayoutContainer>
             <Line />
